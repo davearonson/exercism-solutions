dbb12a661127549fb452bee18e1877fea7bc599d
Dave Aronson <github.2.trex@codosaur.us>
2016-07-29 10:19:17 -0400

    accumulation (mapping) in elixir

---
+++ elixir/accumulate/accumulate.exs
@@ -0,0 +1,28 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate(list, fun) do
+    do_accumulate(list, fun)
+  end
+
+  defp do_accumulate([], fun), do: []
+  defp do_accumulate([head|rest], fun) do
+    [fun.(head)|do_accumulate(rest, fun)]
+  end
+
+end
