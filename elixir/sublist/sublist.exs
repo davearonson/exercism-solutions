5995f58fd1a9ce595ae2b473469c8491213ead48
Dave Aronson <github.2.trex@codosaur.us>
2016-08-11 09:01:04 -0400

    fix sublists in elixir

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,36 +1,29 @@
-# BUG: Sublist.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1])
-# return :sublist, should be :unequal
-
 defmodule Sublist do
   @doc """
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-    do_compare(a, b)
-  end
-
-  defp do_compare(a, b) do
     cond do
-      a === b -> :equal
+      a == b            -> :equal
       is_sublist?(a, b) -> :sublist
       is_sublist?(b, a) -> :superlist
-      true -> :unequal
+      true              -> :unequal
     end
   end
 
-  defp is_sublist?(needle, haystack) do
-    cond do
-      length(haystack) < length(needle) -> false
-      starts_with?(haystack, needle) -> true
-      true -> is_sublist?(needle, tl(haystack))
+  defp is_sublist?(_, []   ), do: false
+  defp is_sublist?(sub, [head|tail]) do
+    case is_sublist_here?(sub, [head|tail]) do
+      false    -> is_sublist?(sub, tail)
+      :no_room -> false
+      true     -> true
     end
   end
 
-  defp starts_with?(_, []), do: true
-  defp starts_with?([], _), do: false
-  defp starts_with?(haystack, needle) do
-    hd(needle) === hd(haystack) && starts_with?(tl(haystack), tl(needle))
-  end
-    
+  defp is_sublist_here?([]    , _     ), do: true
+  defp is_sublist_here?(_     , []    ), do: :no_room
+  defp is_sublist_here?([h|t1], [h|t2]), do: is_sublist_here?(t1, t2)
+  defp is_sublist_here?(_     , _     ), do: false
+
 end
