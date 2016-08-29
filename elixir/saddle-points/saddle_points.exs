9ad538df8f21d429dfd296f7694728fcca055eb4
Dave Aronson <github.2.trex@codosaur.us>
2016-08-28 21:01:20 -0400

    find saddle points in elixir

--- elixir/saddle-points/saddle_points.exs
+++ elixir/saddle-points/saddle_points.exs
@@ -35,6 +35,34 @@ defmodule Matrix do
   """
   @spec saddle_points(String.t()) :: [{integer, integer}]
   def saddle_points(str) do
-
+    rows = rows(str)
+    columns = columns(str)  # more efficient to derive from rows, but....
+    row_maxes = find_candidates(rows, &find_max/1)
+    IO.puts "\nrow_maxes = #{inspect row_maxes}"
+    col_mins = find_candidates(columns, &find_min/1)
+    IO.puts "\ncol_mins = #{inspect col_mins}"
+    MapSet.intersection(col_mins, row_maxes) |> MapSet.to_list
   end
+
+  defp find_candidates(metalist, extreme_finder) do
+    metalist |> Enum.with_index
+             |> Enum.map(extreme_finder)
+             |> List.flatten
+             |> Enum.into(%MapSet{})
+  end
+
+  defp find_max({row, row_num}) do
+    val = Enum.max(row)
+    row |> Enum.with_index
+        |> Enum.filter(&(elem(&1, 0) == val))
+        |> Enum.map(&({row_num, elem(&1, 1)}))
+  end
+
+  defp find_min({col, col_num}) do
+    val = Enum.min(col)
+    col |> Enum.with_index
+        |> Enum.filter(&(elem(&1, 0) == val))
+        |> Enum.map(&({elem(&1, 1), col_num}))
+  end
+
 end
