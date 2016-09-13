34fe5efabb1db2f145c63ee2375e58e6e5730b84
Dave Aronson <github.2.trex@codosaur.us>
2016-09-12 21:29:36 -0400

    crypto square in elixir

---
+++ elixir/crypto-square/crypto_square.exs
@@ -0,0 +1,41 @@
+defmodule CryptoSquare do
+  @doc """
+  Encode string square methods
+  ## Examples
+
+    iex> CryptoSquare.encode("abcd")
+    "ac bd"
+  """
+  @spec encode(String.t) :: String.t
+  def encode(""), do: ""
+  def encode(str) do
+    chars = Regex.scan(~r/[a-zA-Z0-9]/, str |> String.downcase)
+    {_, cols} = best_size(length(chars))
+    chars
+    |> Enum.map(&List.first/1)
+    |> Enum.chunk(cols, cols, [])
+    |> zip_all
+    |> Enum.map(&Enum.join/1)
+    |> Enum.join(" ")
+  end
+
+  defp best_size(len) do
+    root = trunc(:math.sqrt(len))
+    best_size(len, {root,root})
+  end
+  defp best_size(len, {rows, cols}) when rows * cols >= len, do: {rows, cols}
+  defp best_size(len, {rows, rows}), do: best_size(len, {rows , rows+1})
+  defp best_size(len, {rows, cols}), do: best_size(len, {rows+1, cols})
+
+  defp zip_all([next|rest]), do: zip_all(rest, next)
+  defp zip_all([]         , acc), do: acc
+  defp zip_all([next|rest], acc), do: zip_all(rest, my_zip(acc, next))
+
+  # normal zip stops at the first empty one, and this one doesn't,
+  # plus it skips the conversion from nested tuples to one list.
+  defp my_zip(one    , two), do: my_zip(one, two, [])
+  defp my_zip([]     , []     , acc), do: acc |> Enum.reverse
+  defp my_zip([h1|t1], [h2|t2], acc), do: my_zip(t1, t2, [[h1,h2]|acc])
+  defp my_zip([h1|t1], []     , acc), do: my_zip(t1, [], [h1|acc])
+
+end
