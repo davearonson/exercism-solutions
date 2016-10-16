30b1bab972f5c2d6137ef74579b7f5683c8b08cb
Dave Aronson <github.2.trex@codosaur.us>
2016-10-16 11:15:19 -0400

    fix names; make shouted ? a ?

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,29 +1,31 @@
-defmodule Teenager do
+defmodule Bob do
 
   @spec hey(String.t) :: String.t()
   def hey(input) do
     cond do
-      # order is important here, as a shouted question is a shout
-      is_silence?(input)  -> "Fine. Be that way!"
-      is_shout?(input)    -> "Whoa, chill out!"
-      is_question?(input) -> "Sure."
-      true                -> "Whatever."
+      # order is important here; a shouted question
+      # is considered a question, not a shout.
+      # (USED to be considered a shout though.  Harrumph.)
+      silence?(input)  -> "Fine. Be that way!"
+      question?(input) -> "Sure."
+      shout?(input)    -> "Whoa, chill out!"
+      true             -> "Whatever."
     end
   end
 
 
-  defp is_silence?(input) do
+  defp silence?(input) do
     String.rstrip(input) == ""
   end
 
-  defp is_shout?(input) do
+  defp shout?(input) do
     # need to ensure there is at least one letter; "3" is not a shout,
     # even though it matches its upcased version.
     # credit to someone else at the meetup for the alpha regex shortcut.
     String.upcase(input) == input && input =~ ~r/[[:alpha:]]/
   end
 
-  defp is_question?(input) do
+  defp question?(input) do
     String.ends_with?(input, "?")
   end
 
