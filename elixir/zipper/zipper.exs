9ef240d256318069b788b368045bbe1486e10530
Dave Aronson <davearonson@users.noreply.github.com>
2017-09-17 18:18:26 -0400

    implement set_value/left/right

--- elixir/zipper/zipper.exs
+++ elixir/zipper/zipper.exs
@@ -24,13 +24,13 @@ end
 
 defmodule Zipper do
   alias BinTree, as: BT
-  defstruct data: nil, path: nil
+  defstruct data: nil, path: nil, dirs: nil
   @doc """
   Get a zipper focused on the root node.
   """
   @spec from_tree(BT.t) :: Z.t
   def from_tree(bt) do
-    %Zipper{ data: bt, path: [bt] }
+    %Zipper{ data: bt, path: [bt], dirs: [] }
   end
 
   @doc """
@@ -55,7 +55,7 @@ defmodule Zipper do
   @spec left(Z.t) :: Z.t | nil
   def left(%Zipper{path: [%BT{left: nil}|_rest]}), do: nil
   def left(z=%Zipper{path: [%BT{left: lefty}|_rest]}) do
-    %Zipper{ z | path: [lefty|z.path] }
+    %Zipper{ z | path: [lefty|z.path], dirs: [:left|z.dirs] }
   end
 
   @doc """
@@ -63,36 +63,57 @@ defmodule Zipper do
   """
   def right(%Zipper{path: [%BT{right: nil}|_rest]}), do: nil
   def right(z=%Zipper{path: [%BT{right: righty}|_rest]}) do
-    %Zipper{ z | path: [righty|z.path] }
+    %Zipper{ z | path: [righty|z.path], dirs: [:right|z.dirs] }
   end
 
   @doc """
   Get the parent of the focus node, if any.
   """
   @spec up(Z.t) :: Z.t
-  def up(%Zipper{path: [_just_one]}), do: nil
-  def up(z=%Zipper{path: [_head|rest]}) do
-    %Zipper{ z | path: rest }
+  def up(%Zipper{dirs: []}), do: nil
+  def up(z=%Zipper{path: [_cur|path_rest], dirs: [_curdir|dirs_rest]}) do
+    %Zipper{ z | path: path_rest, dirs: dirs_rest }
   end
 
   @doc """
   Set the value of the focus node.
   """
   @spec set_value(Z.t, any) :: Z.t
-  def set_value(z, v) do
+  def set_value(z=%Zipper{path: [head|rest]}, v) do
+    new_head = %BT{ head | value: v }
+    new_data = update_tree(rest, z.dirs, new_head)
+    %Zipper{ z | data: new_data,
+                 path: [new_head | rest] }
   end
 
   @doc """
   Replace the left child tree of the focus node.
   """
   @spec set_left(Z.t, BT.t) :: Z.t
-  def set_left(z, l) do
+  def set_left(z=%Zipper{path: [head|rest]}, l) do
+    new_head = %BT{ head | left: l }
+    new_data = update_tree(rest, z.dirs, new_head)
+    %Zipper{ z | data: new_data, path: [new_head | rest] }
   end
 
   @doc """
   Replace the right child tree of the focus node.
   """
   @spec set_right(Z.t, BT.t) :: Z.t
-  def set_right(z, r) do
+  def set_right(z=%Zipper{path: [head|rest]}, l) do
+    new_head = %BT{ head | right: l }
+    new_data = update_tree(rest, z.dirs, new_head)
+    %Zipper{ z | data: new_data, path: [new_head | rest] }
   end
+
+
+  defp update_tree([], [], sub), do: sub
+  defp update_tree([head|rest], [last_dir|prev_dirs], sub) do
+    new_sub = case last_dir do
+                :left -> %BT{ head | left: sub }
+                :right -> %BT{ head | right: sub }
+              end
+    update_tree(rest, prev_dirs, new_sub)
+  end
+
 end
