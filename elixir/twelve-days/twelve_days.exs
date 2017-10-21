22e0cca266b23a42eeea7d0803d39b61d019d34e
Dave Aronson <davearonson@users.noreply.github.com>
2017-10-21 11:08:42 -0400

    naive impl to get 1st test passing

---
+++ elixir/twelve-days/twelve_days.exs
@@ -0,0 +1,63 @@
+defmodule TwelveDays do
+
+  @gifts {
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming"
+  }
+
+  @numbers {
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+  }
+
+  @doc """
+  Given a `number`, return the song's verse for that specific day, including
+  all gifts for previous days in the same line.
+  """
+  @spec verse(number :: integer) :: String.t()
+  def verse(number) do
+    index = number - 1
+    "On the #{elem(@numbers, index)} day of Christmas my true love gave to me, #{gift_list(index)}."
+  end
+
+  @doc """
+  Given a `starting_verse` and an `ending_verse`, return the verses for each
+  included day, one per line.
+  """
+  @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
+  def verses(starting_verse, ending_verse) do
+  end
+
+  @doc """
+  Sing all 12 verses, in order, one verse per line.
+  """
+  @spec sing():: String.t()
+  def sing do
+  end
+
+  defp gift_list(number) do
+    elem(@gifts, number)
+  end
+
+end
+
