845492d5388c77ad9b172963041a9f6e0ed1658b
Dave Aronson <davearonson@users.noreply.github.com>
2017-11-22 17:05:57 -0500

    simple matrix ops in elixir

---
+++ elixir/matrix/matrix.exs
@@ -0,0 +1,68 @@
+defmodule Matrix do
+  defstruct matrix: nil
+
+  @doc """
+  Convert an `input` string, with rows separated by newlines and values
+  separated by single spaces, into a `Matrix` struct.
+  """
+  @spec from_string(input :: String.t()) :: %Matrix{}
+  def from_string(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&(String.split(&1, " ")))
+    |> Enum.map(&make_elements_integers/1)
+  end
+
+  # need this to be a separate function
+  # cuz elixir won't let us nest captures
+  defp make_elements_integers(row) do
+    row |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Write the `matrix` out as a string, with rows separated by newlines and
+  values separated by single spaces.
+  """
+  @spec to_string(matrix :: %Matrix{}) :: String.t()
+  def to_string(matrix) do
+    matrix
+    |> Enum.map(&(Enum.join(&1, " ")))
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Given a `matrix`, return its rows as a list of lists of integers.
+  """
+  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
+  def rows(matrix) do
+    matrix
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the row at `index`.
+  """
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def row([ head|_tail], 0), do: head
+  def row([_head| tail], index), do: row(tail, index-1)
+
+  @doc """
+  Given a `matrix`, return its columns as a list of lists of integers.
+  """
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
+  def columns(matrix) do
+    matrix |> transpose |> rows
+  end
+
+  defp transpose(matrix) do
+    matrix |> List.zip |> Enum.map(&Tuple.to_list/1)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the column at `index`.
+  """
+  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def column(matrix, index) do
+    matrix |> transpose |> row(index)
+  end
+end
+
