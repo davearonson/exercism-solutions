8ba0acd1df5739320efb70b618d86c4821fa9b8c
Dave Aronson <davearonson@users.noreply.github.com>
2017-12-03 12:45:56 -0500

    make return value style more consistant

--- elixir/all-your-base/all-your-base.exs
+++ elixir/all-your-base/all-your-base.exs
@@ -9,9 +9,9 @@ defmodule AllYourBase do
   def convert([],      _base_a, _base_b), do: nil
   def convert(_digits,  base_a,  base_b) when base_a < 2 or base_b < 2, do: nil
   def convert( digits,  base_a,  base_b) do
-    with {:ok, value} <- read_digits_in_base(digits, base_a, 0),
-         new_digits   <- write_digits_in_base(value, base_b, []),
-         final_digits <- fix_digit_list(new_digits)
+    with {:ok, value}        <- read_digits_in_base(digits, base_a, 0),
+         {:ok, new_digits}   <- write_digits_in_base(value, base_b, []),
+         {:ok, final_digits} <- fix_digit_list(new_digits)
     do
       final_digits
     else
@@ -26,12 +26,12 @@ defmodule AllYourBase do
     read_digits_in_base(tail, base, acc * base + head)
   end
 
-  defp write_digits_in_base(    0, _base, acc), do: acc
+  defp write_digits_in_base(    0, _base, acc), do: {:ok, acc}
   defp write_digits_in_base(value,  base, acc)  do
     write_digits_in_base(div(value, base), base, [rem(value, base)|acc])
   end
 
-  defp fix_digit_list([]    ), do: [0]
-  defp fix_digit_list(digits), do: digits
+  defp fix_digit_list([]    ), do: {:ok, [0]}
+  defp fix_digit_list(digits), do: {:ok, digits}
 
 end
