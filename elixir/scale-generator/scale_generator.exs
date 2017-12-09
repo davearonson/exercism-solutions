b6a25cf473d20846df6130ad3b28d94ff756c663
Dave Aronson <davearonson@users.noreply.github.com>
2017-12-09 18:56:05 -0500

    scale generator in elixir

--- elixir/scale-generator/scale_generator.exs
+++ elixir/scale-generator/scale_generator.exs
@@ -15,6 +15,8 @@ defmodule ScaleGenerator do
   """
   @spec step(scale :: list(String.t()), tonic :: String.t(), step :: String.t()) :: list(String.t())
   def step(scale, tonic, step) do
+    scale
+    |> Enum.at(rem(find_value(scale, tonic) + step_size(step), 12))
   end
 
   @doc """
@@ -33,6 +35,8 @@ defmodule ScaleGenerator do
   """
   @spec chromatic_scale(tonic :: String.t()) :: list(String.t())
   def chromatic_scale(tonic \\ "C") do
+    generate_scale(~w(C C# D D# E F F# G G# A A# B),
+                   tonic |> String.upcase)
   end
 
   @doc """
@@ -49,6 +53,8 @@ defmodule ScaleGenerator do
   """
   @spec flat_chromatic_scale(tonic :: String.t()) :: list(String.t())
   def flat_chromatic_scale(tonic \\ "C") do
+    generate_scale(~w(C Db D Eb E F Gb G Ab A Bb B),
+                   tonic |> upcase_only_first_letter)
   end
 
   @doc """
@@ -63,6 +69,11 @@ defmodule ScaleGenerator do
   """
   @spec find_chromatic_scale(tonic :: String.t()) :: list(String.t())
   def find_chromatic_scale(tonic) do
+    if ~w(F Bb Eb Ab Db Gb d g c f bb eb) |> find_value(tonic) do
+      flat_chromatic_scale(tonic)
+    else
+      chromatic_scale(tonic)
+    end
   end
 
   @doc """
@@ -78,6 +89,36 @@ defmodule ScaleGenerator do
   """
   @spec scale(tonic :: String.t(), pattern :: String.t()) :: list(String.t())
   def scale(tonic, pattern) do
+    [(tonic |> upcase_only_first_letter)|
+     apply_pattern(String.graphemes(pattern), find_chromatic_scale(tonic), [])]
   end
+
+
+  defp apply_pattern([head|tail], scale, acc) do
+    idx = step_size(head)
+    apply_pattern(tail, scale |> Enum.drop(idx), [scale |> Enum.at(idx)|acc])
+  end
+  defp apply_pattern([], _scale, acc), do: acc |> Enum.reverse
+
+  defp find_value(list, value) do
+    Enum.find_index(list , fn(element) -> element == value end)
+  end
+
+  defp generate_scale(notes, tonic) do
+    tonic_index = notes |> find_value(tonic)
+    (notes |> Enum.drop(tonic_index)) ++
+      (notes |> Enum.take(tonic_index)) ++
+      [tonic]
+  end
+
+  defp step_size(step), do: find_value(~w(m M A), step) + 1
+
+  defp upcase_only_first_letter(str) do
+    str
+    |> String.graphemes
+    |> (fn([h|t]) -> [String.upcase(h) | t] end).()
+    |> Enum.join
+  end
+
 end
 
