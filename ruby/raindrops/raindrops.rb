8f8777e4716fd309a600e109989223aadf8c86f8
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-12 16:07:49 -0400

    more functional, less stateful

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,17 +1,14 @@
 class Raindrops
 
   def self.convert(num)
-    output = ""
-    NOISES.each { |factor, noise| output << noise if num % factor == 0 }
-    output == "" ? num.to_s : output
+    output = NOISES.map { |factor, noise| noise if num % factor == 0 }
+    # trick: nils (from above) are falsey, and "arr.any?" means
+    # "arr.any? { |elt| elt.truthy? }", NOT "arr.size > 0"!
+    return output.any? ? output.join : num.to_s
   end
 
   private
 
-  NOISES = {
-    3 => "Pling",
-    5 => "Plang",
-    7 => "Plong"
-  }
+  NOISES = { 3 => "Pling", 5 => "Plang", 7 => "Plong" }
 
 end
