d0ee3f31cbe09a2cfe40d187baf58c07d6ecea96
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-17 15:59:39 -0400

    update name (Exercism dictates); rm unused var

--- python/raindrops/daves_raindrops_test.py
+++ python/raindrops/daves_raindrops_test.py
@@ -1,40 +1,40 @@
 import pytest
 
-from raindrops import raindrops
+from raindrops import convert
 
 
 # basic functionality:
 
 def test_multiple_of_none_becomes_string():
-    assert raindrops(1) == "1"
+    assert convert(1) == "1"
 
 def test_3_is_pling():
-    assert raindrops(6) == "Pling"
+    assert convert(6) == "Pling"
 
 def test_5_is_plang():
-    assert raindrops(5) == "Plang"
+    assert convert(5) == "Plang"
 
 def test_7_is_plong():
-    assert raindrops(7) == "Plong"
+    assert convert(7) == "Plong"
 
 def test_3_and_5_make_plingplang():  # not plangpling
-    assert raindrops(30) == "PlingPlang"
+    assert convert(30) == "PlingPlang"
 
 def test_3_and_7_make_plingplong():  # not plongpling
-    assert raindrops(42) == "PlingPlong"
+    assert convert(42) == "PlingPlong"
 
 def test_5_and_7_make_plangplong():  # not plongplang
-    assert raindrops(70) == "PlangPlong"
+    assert convert(70) == "PlangPlong"
 
 def test_all_together_make_plingplangplong():
-    assert raindrops(210) == "PlingPlangPlong"  # not any other order
+    assert convert(210) == "PlingPlangPlong"  # not any other order
 
 
 # light bulletproofing:
 
 def test_raising_error_with_non_integer_input():
-    with pytest.raises(ValueError) as exception_info:
-        raindrops("blah")
+    with pytest.raises(ValueError):
+        convert("blah")
         assert False, "Failed to raise an error with a non-integer input!"
     assert True
 
@@ -43,22 +43,22 @@ def test_raising_error_with_non_integer_input():
 
 # we could possibly consider N.0 equivalent to N, but for now, no
 def test_raising_error_with_int_valued_float():
-    with pytest.raises(ValueError) as exception_info:
-        raindrops(4.0)
+    with pytest.raises(ValueError):
+        convert(4.0)
         assert False, "Failed to raise an error with a rounded float!"
     assert True
 
 def test_works_with_zero():
-    assert raindrops(0) == "PlingPlangPlong"
+    assert convert(0) == "PlingPlangPlong"
 
 def test_works_with_negative_non_multiples():
-    assert raindrops(-4) == "-4"
+    assert convert(-4) == "-4"
 
 def test_works_with_negative_multiples_of_one_trigger():
-    assert raindrops(-50) == "Plang"
+    assert convert(-50) == "Plang"
 
 def test_works_with_negative_multiples_of_two_triggers():
-    assert raindrops(-42) == "PlingPlong"
+    assert convert(-42) == "PlingPlong"
 
 def test_works_with_negative_multiples_of_all():
-    assert raindrops(-210) == "PlingPlangPlong"
+    assert convert(-210) == "PlingPlangPlong"
--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,8 +1,6 @@
-from collections import OrderedDict 
-
 NOISES = ((3, "Pling"), (5, "Plang"), (7, "Plong"))
 
-def raindrops(number):
+def convert(number):
     if not isinstance(number, int): raise ValueError
     result = [noise for trigger, noise in NOISES if number % trigger == 0]
     return "".join(result) if result else str(number)  # empty list is falsy
