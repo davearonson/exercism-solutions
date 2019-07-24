96e6c74e0ce2623a7b35acf7eebc73066e1a32b5
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-24 19:44:15 -0400

    better solution to isograms in python

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,4 @@
 def is_isogram(string):
     if not isinstance(string, str): raise ValueError
-    alfa_chars = [c for c in string.lower() if c.isalpha()]
+    alfa_chars = filter(str.isalpha, string.lower())
     return len(set(alfa_chars)) == len(alfa_chars)
