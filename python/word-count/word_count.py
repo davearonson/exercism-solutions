52d980b932cbeb1ddc16e8a0e8938bd15457f47b
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-26 13:46:55 -0400

    use counter module

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,10 +1,8 @@
+from collections import Counter
 import re
 
 def count_words(sentence):
     words = re.split(r"[^a-z0-9']", sentence.lower())
     words = filter(lambda w : w, words)
     words = map(lambda w : w.strip("'"), words)
-    tally = {}
-    for word in words:
-        tally[word] = tally[word] + 1 if word in tally else 1
-    return tally
+    return Counter(words)
