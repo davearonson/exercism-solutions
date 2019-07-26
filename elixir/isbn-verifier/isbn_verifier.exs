2de1ff0fc09f12eda60a2d821a831e27ee966ff1
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-26 12:17:09 -0400

    check isbn format after dash removal, w/ regex

--- elixir/isbn-verifier/isbn_verifier.exs
+++ elixir/isbn-verifier/isbn_verifier.exs
@@ -13,24 +13,19 @@ defmodule ISBNVerifier do
   """
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
-    {status, result} =
-      isbn
-      |> remove_legit_dashes
-      |> String.graphemes
-      |> checksum(1, 0)
-    status == :ok && rem(result, 11) == 0
+    new_isbn = remove_legit_dashes(isbn)
+    if String.match?(new_isbn, ~r/^\d{9}(\d|X)$/) do
+      try_checksum(new_isbn)
+    else
+      false
+    end
   end
 
-  defp checksum([]       , 11, acc), do: {:ok, acc } # just right length, ended
-  defp checksum([]       , _ , _  ), do: {:error, "too short" }
-  defp checksum(_        , 11, _  ), do: {:error, "too long" }
-  defp checksum(["X"]    , 10, acc), do: checksum(["10"], 10, acc)  # spcl case
-  defp checksum([c|_]    , _ , _  ) when c < "0" or c > "9", do:
-    {:error, "non-digit found, other than special exceptions"}
+  defp checksum([]       , _ , acc), do: {:ok, acc }
   defp checksum([digit|rest], digit_number, acc) do
-    # in the case of a final X, digit will not really be a
-    # single digit when we get here, but "10"; see "X" case above
-    new_acc = acc + String.to_integer(digit) * (11 - digit_number)
+    # we've ensured, by regex call up top, that the X can only be last
+    digit_value = if digit == "X", do: 10, else: String.to_integer(digit)
+    new_acc = acc + digit_value * (11 - digit_number)
     checksum(rest, digit_number + 1, new_acc)
   end
 
@@ -38,14 +33,23 @@ defmodule ISBNVerifier do
   # there are many styles but they all have in common that
   # the only legit place for a dash is between two digits
   defp remove_legit_dashes(string) do
-    # Why dip, er, replace twice?  If you have one digit between two
-    # dashes, replace will pick up from AFTER the replacement,
+    # Why dip, er, replace twice?  If you have one digit between
+    # two dashes, replace will pick up from AFTER the replacement,
     # so it won't see the second dash as being preceded by a digit.  :-(
     # Could solve this with a more complex but efficient function,
-    # but I don't wanna right now.
+    # but I don't wanna right now.  As is, this passes the ISBN rules
+    # even for many non-English-speaking locations.
     string
       |> String.replace(~r/(\d)-(\d|X)/, "\\1\\2")
       |> String.replace(~r/(\d)-(\d|X)/, "\\1\\2")
+  end
+
+  defp try_checksum(isbn) do
+    {status, result} =
+      isbn
+      |> String.graphemes
+      |> checksum(1, 0)
+    status == :ok && rem(result, 11) == 0
   end
 
 end
