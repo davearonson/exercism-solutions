794be649610fe723c6bf90b466120589950ba56b
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-26 14:08:39 -0400

    simplify blank 'word' removal

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,5 +3,6 @@ import re
 
 def count_words(sentence):
     words = re.split(r"[^a-z0-9']", sentence.lower())
-    words = map(lambda w : w.strip("'"), [w for w in words if w])
+    words = map(lambda w : w.strip("'"), words)
+    words = filter(lambda w : w, words)
     return Counter(words)
