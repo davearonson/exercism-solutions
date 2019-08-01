81fce492c21ef3b72fcec23c876be4656de705a9
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-31 21:31:46 -0400

    dump hours from clock in python

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,17 +1,16 @@
 class Clock(object):
     def __init__(self, hour, minute):
-        total = hour * 60 + minute
-        self._hour = (total // 60) % 24
-        self._minute = total % 60
+        self._minutes = (hour * 60 + minute) % 1440  # 24 * 60
 
     def __repr__(self):
-        return "%02d:%02d" % (self._hour, self._minute)
+        hrs, mins = divmod(self._minutes, 60)
+        return "%02d:%02d" % (hrs % 24, mins)
 
     def __eq__(self, other):
-        return self._hour == other._hour and self._minute == other._minute
+        return self._minutes == other._minutes
 
     def __add__(self, minutes):
-        return self.__class__(self._hour, self._minute + minutes)
+        return self.__class__(0, self._minutes + minutes)
 
     def __sub__(self, minutes):
-        return self.__class__(self._hour, self._minute - minutes)
+        return self.__class__(0, self._minutes - minutes)
