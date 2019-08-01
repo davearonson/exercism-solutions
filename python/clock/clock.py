2f61c9fbdc94339fcead28a2ca30acff6c7f739d
Dave Aronson <davearonson@users.noreply.github.com>
2019-07-31 21:20:17 -0400

    clock in python

---
+++ python/clock/clock.py
@@ -0,0 +1,19 @@
+import math
+
+class Clock(object):
+    def __init__(self, hour, minute):
+        total = hour * 60 + minute
+        self._hour = math.floor(total / 60) % 24
+        self._minute = total % 60
+
+    def __repr__(self):
+        return f"{'%02d' % self._hour}:{'%02d' % self._minute}"
+
+    def __eq__(self, other):
+        return self._hour == other._hour and self._minute == other._minute
+
+    def __add__(self, minutes):
+        return self.__class__(self._hour, self._minute + minutes)
+
+    def __sub__(self, minutes):
+        return self.__class__(self._hour, self._minute - minutes)
