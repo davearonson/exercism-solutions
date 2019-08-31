51cdef1dc218724b19f2bb8c2f3df6a35affcbbc
Dave Aronson <davearonson@users.noreply.github.com>
2019-08-31 13:17:35 -0400

    use zip instead of homegrown kluge

--- elixir/transpose/lib/transpose.ex
+++ elixir/transpose/lib/transpose.ex
@@ -16,14 +16,12 @@ defmodule Transpose do
   "AD\nBE\n F"
   """
 
-
   # don't use a char 'cuz it might be actually used
-  @padsub 0
-
+  @pad -1
 
   @spec transpose(String.t()) :: String.t()
-  def transpose(""), do: ""  # smartass!
-  def transpose(input) do
+  def transpose(""   ), do: ""  # smartass!
+  def transpose(input)  do
     rows = String.split(input, "\n")
     max_len = 
       rows
@@ -32,43 +30,38 @@ defmodule Transpose do
     rows
     |> Enum.map(&String.graphemes/1)
     |> Enum.map(&(pad_to(&1, max_len)))
-    |> do_transpose([])
-    |> Enum.map(&(unpad(&1, false, [])))
+    |> Enum.zip
+    |> Enum.map(&fixup/1)
     |> Enum.map(&Enum.join/1)
     |> Enum.join("\n")
   end
 
-
-  def pad_to(arr, len) do
+  defp pad_to(arr, len) do
     cur_len = length(arr)
     if cur_len >= len do
       arr
     else
-      arr ++ List.duplicate(@padsub, len - cur_len)
+      # yes this can be inefficient with long input strings,
+      # but we're not worried about that in this use-case
+      arr ++ List.duplicate(@pad, len - cur_len)
     end
   end
 
-
-  def do_transpose([[]|_], acc), do: Enum.reverse(acc)
-  def do_transpose(rows  , acc)  do
-    { firsts, rests } = pop_rows(rows, [], [])
-    do_transpose(rests, [firsts|acc])
+  defp fixup(tup) do
+    tup
+    |> Tuple.to_list
+    |> Enum.reverse
+    |> do_unpad(false, [])
+    # do not reverse here; between fixup and do_unpad we need an odd number
+    # of explicit reversals (to counter the implicit one from do_unpad),
+    # the one above is needed, and the other solution, of reversing both here
+    # and at the end of do_unpad, would be redundant.
   end
 
-
-  def pop_rows([], firsts, rests) do
-    { firsts, Enum.reverse(rests) }
-  end
-
-  def pop_rows([first_row|more_rows], firsts, rests) do
-    { [first], rest } = Enum.split(first_row, 1)
-    pop_rows(more_rows, [first|firsts], [rest|rests])
-  end
-
-
-  def unpad([@padsub|rest], false, acc), do: unpad(rest, false, acc)
-  def unpad([@padsub|rest], true , acc), do: unpad(rest, true , [" "  |acc])
-  def unpad([first  |rest], _    , acc), do: unpad(rest, true , [first|acc])
-  def unpad([],             _    , acc), do: acc
+  # middle arg means, have we seen something yet that's not a pad substitute?
+  defp do_unpad([@pad|rest], false, acc), do: do_unpad(rest, false, acc)
+  defp do_unpad([@pad|rest], true , acc), do: do_unpad(rest, true,  [" " |acc])
+  defp do_unpad([last|rest], _    , acc), do: do_unpad(rest, true,  [last|acc])
+  defp do_unpad([]         , _    , acc), do: acc  # see comment in fixup
 
 end
