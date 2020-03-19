44fc6b53398e7117f9e3b1d57e7eb6c211e0f2f5
Dave Aronson <davearonson@users.noreply.github.com>
2020-03-19 15:29:28 -0400

    resistor colors in elixir

---
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -0,0 +1,26 @@
+defmodule ResistorColor do
+  @moduledoc false
+
+  @colors [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ]
+
+  @spec colors() :: list(String.t())
+  def colors do
+    @colors
+  end
+
+  @spec code(String.t()) :: integer()
+  def code(color) do
+    Enum.find_index(@colors, fn c -> c == color end)
+  end
+end
