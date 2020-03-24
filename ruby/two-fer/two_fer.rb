cb84727dc62927e07d933c207360916f86552485
Dave Aronson <davearonson@users.noreply.github.com>
2020-03-24 15:03:21 -0400

    two-fer in ruby

---
+++ ruby/two-fer/two_fer.rb
@@ -0,0 +1,13 @@
+=begin
+Write your code for the 'Two Fer' exercise in this file. Make the tests in
+`two_fer_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/two-fer` directory.
+=end
+
+module TwoFer
+  def self.two_fer(name = "you")
+    "One for #{name}, one for me."
+  end
+end
