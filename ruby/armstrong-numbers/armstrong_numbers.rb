5b969676812b7c0abcace5af5c4d98cd60f41e38
Dave Aronson <davearonson@users.noreply.github.com>
2020-03-24 15:11:23 -0400

    armstrong numbers in ruby

---
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -0,0 +1,15 @@
+=begin
+Write your code for the 'Armstrong Numbers' exercise in this file. Make the tests in
+`armstrong_numbers_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/armstrong-numbers` directory.
+=end
+
+module ArmstrongNumbers
+  def self.include?(n)
+    chars = n.to_s.chars
+    num_chars = chars.count
+    chars.map(&:to_i).map { |i| i ** num_chars }.inject(:+) == n
+  end
+end
