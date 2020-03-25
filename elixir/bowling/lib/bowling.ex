9abdf65fb7d25c3a496f8b0abf7779d9d87dfcf9
Dave Aronson <davearonson@users.noreply.github.com>
2020-03-25 19:43:06 -0400

    use constants instead of magic numbers, duh

--- elixir/bowling/lib/bowling.ex
+++ elixir/bowling/lib/bowling.ex
@@ -33,62 +33,63 @@ defmodule Bowling do
 
   @spec roll(any, integer) :: any | String.t()
 
-  @game_over_error     {:error, "Cannot roll after game is over"}
-  @too_many_pins_error {:error, "Pin count exceeds pins on the lane"}
+  @game_over_error      {:error, "Cannot roll after game is over"}
+  @frame_max            10  # NORMAL frames; bonuses could get "frame" up to 12
+  @pins_max             10
+  @too_many_pins_error  {:error, "Pin count exceeds pins on the lane"}
 
   def roll(_, roll) when roll < 0, do: {:error, "Negative roll is invalid"}
-  def roll(_, roll) when roll > 10, do: @too_many_pins_error
+  def roll(_, roll) when roll > @pins_max, do: @too_many_pins_error
 
-  def roll(%{first_roll: false, rolls: [last|_rest]}, roll)
-      when last + roll > 10 do
-    @too_many_pins_error
-  end
+  def roll(%{first_roll: false, rolls: [last | _rest]}, roll)
+      when last + roll > @pins_max,
+      do: @too_many_pins_error
 
-  # after final strike, strike in 11th frame, and 12th -- NOT TESTED!
-  def roll(%{frame: 13, rolls: [10|[10|_rest]]}, _), do: @game_over_error
+  # after final strike, strike in 11th frame, and 12th --
+  # NOT TESTED by Exercism's supplied test suite
+  def roll(%{frame: 13, rolls: [@pins_max | [@pins_max | _rest]]}, _),
+    do: @game_over_error
 
   # after final strike, and non-strike (and whatever, even spare) in 11th frame
-  def roll(%{frame: 12, rolls: [_last|[second|_rest]]}, _) when second != 10 do
-    @game_over_error
-  end
+  def roll(%{frame: 12, rolls: [_last | [second | _rest]]}, _)
+      when second != @pins_max,
+      do: @game_over_error
 
   # after final spare, and its bonus roll
-  def roll(%{first_roll: false, frame: 11, rolls: [_|[spare1|[spare2]]]}, _)
-      when spare1 + spare2 == 10 do
-    @game_over_error
-  end
+  def roll(%{first_roll: false, frame: 11, rolls: [_ | [spare1 | [spare2]]]}, _)
+      when spare1 + spare2 == @pins_max,
+      do: @game_over_error
 
   # after normal final frame
-  def roll(%{frame: 11, rolls: [last|[second|_rest]]}, _)
-      when last < 10 and last + second < 10 do
-    @game_over_error
-  end
+  def roll(%{frame: 11, rolls: [last | [second | _rest]]}, _)
+      when last < @pins_max and last + second < @pins_max,
+      do: @game_over_error
 
   # strike
-  def roll(game = %{first_roll: true, frame: frame}, 10) when frame < 10 do
-    new_game(game, true, game.mult_2 + 1, 2, 10)
-  end
+  def roll(game = %{first_roll: true, frame: frame}, @pins_max)
+      when frame < @frame_max,
+      do: record_roll(game, true, game.mult_2 + 1, 2, @pins_max)
 
   # spare
-  def roll(game = %{first_roll: false, frame: frame, rolls: [last|_]}, roll)
-      when roll + last == 10 and frame < 10 do
-    new_game(game, true, game.mult_2 + 1, 1, roll)
-  end
+  def roll(game = %{first_roll: false, frame: frame, rolls: [last | _]}, roll)
+      when roll + last == @pins_max and frame < @frame_max,
+      do: record_roll(game, true, game.mult_2 + 1, 1, roll)
 
   # anything else
   def roll(game, roll) do
-    new_first = (not game.first_roll) or roll == 10
-    new_game(game, new_first, game.mult_2, 1, roll)
+    new_first = roll == @pins_max or not game.first_roll
+    record_roll(game, new_first, game.mult_2, 1, roll)
   end
 
-  defp new_game(game, first_roll, mult_1, mult_2, roll) do
-    %__MODULE__{game |
-      first_roll: first_roll,
-      frame:      game.frame + (if first_roll, do: 1, else: 0),
-      mult_1:     mult_1,
-      mult_2:     mult_2,
-      rolls:      [roll|game.rolls],
-      score:      game.score + roll * game.mult_1
+  defp record_roll(game, first_roll, mult_1, mult_2, roll) do
+    %__MODULE__{
+      game
+      | first_roll: first_roll,
+        frame:      game.frame + (if first_roll, do: 1, else: 0),
+        mult_1:     mult_1,
+        mult_2:     mult_2,
+        rolls:      [roll | game.rolls],
+        score:      game.score + roll * game.mult_1
     }
   end
 
@@ -100,26 +101,26 @@ defmodule Bowling do
   @must_end_error {:error, "Score cannot be taken until the end of the game"}
 
   @spec score(any) :: integer | String.t()
-  def score(%{frame: frame}) when frame <= 10, do: @must_end_error
+  def score(%{frame: frame}) when frame <= @frame_max,
+    do: @must_end_error
 
   # awaiting bonus ball for a final spare
   # can't incorporate this into above cuz unstarted won't have last & second
-  def score(%{first_roll: true, frame: 11, rolls: [last|[next|_rest]]})
-      when last + next == 10 do
-    @must_end_error
-  end
+  def score(%{first_roll: true, frame: 11, rolls: [last | [next | _rest]]})
+      when last + next == @pins_max,
+      do: @must_end_error
 
   # awaiting first bonus ball for a final strike
-  def score(%{frame: 11, rolls: [10|_rest]}), do: @must_end_error
+  def score(%{frame: 11, rolls: [@pins_max | _rest]}),
+    do: @must_end_error
 
   # awaiting second bonus ball for a final strike after 1st was also strike
-  def score(%{frame: 12, rolls: [10|[10|_rest]]}), do: @must_end_error
+  def score(%{frame: 12, rolls: [@pins_max | [@pins_max | _rest]]}),
+    do: @must_end_error
 
   # awaiting second bonus ball for a final strike after 1st was normal
-  def score(%{frame: 11, rolls: [_last|[10|_rest]]}) do
-    @must_end_error
-  end
+  def score(%{frame: 11, rolls: [_last | [@pins_max | _rest]]}),
+    do: @must_end_error
 
   def score(game), do: game.score
-
 end
