09899f43c61fab0f13c457d76c445f55ea76a343
Dave Aronson <davearonson@users.noreply.github.com>
2020-04-05 17:16:06 -0400

    use mix format to help w/ style

--- elixir/dot-dsl/lib/dot.ex
+++ elixir/dot-dsl/lib/dot.ex
@@ -1,9 +1,9 @@
 defmodule Dot do
   defmacro graph(ast) do
     walk(ast, %Graph{})
-    |> sort_attrs
-    |> sort_nodes
-    |> Macro.escape
+    |> sort_attrs()
+    |> sort_nodes()
+    |> Macro.escape()
   end
 
   defp walk({:do, {:__block__, _, contents}}, graph) do
@@ -13,13 +13,13 @@ defmodule Dot do
   defp walk({:do, contents}, graph), do: walk(contents, graph)
 
   defp walk(list, graph) when is_list(list),
-      do: Enum.reduce(list, graph, &walk/2)
+    do: Enum.reduce(list, graph, &walk/2)
 
   defp walk(non_tuple, _graph) when not is_tuple(non_tuple),
-      do: raise ArgumentError
+    do: raise(ArgumentError)
 
   defp walk(missized_tuple, _graph) when tuple_size(missized_tuple) != 3,
-      do: raise ArgumentError
+    do: raise(ArgumentError)
 
   defp walk({:--, [_loc], [{node_a, _, _}, {node_b, _, attrs}]}, graph) do
     %Graph{graph | edges: [{node_a, node_b, make_attrs(attrs)} | graph.edges]}
@@ -34,16 +34,19 @@ defmodule Dot do
   end
 
   defp make_attrs([attrs]) do
-    if Keyword.keyword?(attrs), do: attrs, else: raise ArgumentError
+    if Keyword.keyword?(attrs), do: attrs, else: raise(ArgumentError)
   end
+
   defp make_attrs(nil), do: []
-  defp make_attrs(_)  , do: raise ArgumentError
+  defp make_attrs(_)  , do: raise(ArgumentError)
 
   defp sort_attrs(graph = %{attrs: [_any | _more]}),
-      do: %Graph{graph | attrs: Enum.sort(graph.attrs)}
+    do: %Graph{graph | attrs: Enum.sort(graph.attrs)}
+
   defp sort_attrs(graph), do: graph
 
   defp sort_nodes(graph = %{nodes: [_any | _more]}),
-      do: %Graph{graph | nodes: Enum.sort(graph.nodes)}
+    do: %Graph{graph | nodes: Enum.sort(graph.nodes)}
+
   defp sort_nodes(graph), do: graph
 end
