7d10426fb22e541e896010701e0210d0fcaa0c8e
Dave Aronson <davearonson@users.noreply.github.com>
2020-04-08 21:54:06 -0400

    adapt to use Macro.prewalk

--- elixir/dot-dsl/lib/dot.ex
+++ elixir/dot-dsl/lib/dot.ex
@@ -1,44 +1,46 @@
 defmodule Dot do
-  defmacro graph(ast) do
-    walk(ast, %Graph{})
+  defmacro graph([do: ast]) do
+    {_ast, res} = Macro.prewalk(ast, %Graph{}, &handle_node/2)
+    res
     |> sort_attrs()
     |> sort_nodes()
     |> Macro.escape()
   end
 
-  defp walk({:do, {:__block__, _, contents}}, graph) do
-    walk(contents, graph)
+  # just don't hand back the node, cuz we may have handled some sub-nodes
+  defp handle_node(node, graph), do: {:whatever, do_handle_node(node, graph)}
+
+  defp do_handle_node({:__block__, [_attrs], contents}, graph),
+    do: Enum.reduce(contents, graph, &do_handle_node/2)
+
+  defp do_handle_node({:--, [_loc], [{a, _, _}, {b, _, attrs}]}, graph) do
+    %Graph{graph | edges: [{a, b, make_attrs(attrs)} | graph.edges]}
   end
 
-  defp walk({:do, contents}, graph), do: walk(contents, graph)
-
-  defp walk(list, graph) when is_list(list),
-    do: Enum.reduce(list, graph, &walk/2)
-
-  defp walk(non_tuple, _graph) when not is_tuple(non_tuple),
-    do: raise(ArgumentError)
-
-  defp walk(missized_tuple, _graph) when tuple_size(missized_tuple) != 3,
-    do: raise(ArgumentError)
-
-  defp walk({:--, [_loc], [{node_a, _, _}, {node_b, _, attrs}]}, graph) do
-    %Graph{graph | edges: [{node_a, node_b, make_attrs(attrs)} | graph.edges]}
-  end
-
-  defp walk({:graph, [_loc], attrs}, graph) do
+  defp do_handle_node({:graph, [_loc], attrs}, graph) do
     %Graph{graph | attrs: make_attrs(attrs) ++ graph.attrs}
   end
 
-  defp walk({atom, [_loc], attrs}, graph) do
+  defp do_handle_node({atom, [_loc], attrs}, graph) when is_atom(atom) do
     %Graph{graph | nodes: [{atom, make_attrs(attrs)} | graph.nodes]}
   end
 
+  defp do_handle_node(node, _graph),
+    do: raise(ArgumentError, message: "Dunno how to handle #{inspect(node)}")
+
   defp make_attrs([attrs]) do
-    if Keyword.keyword?(attrs), do: attrs, else: raise(ArgumentError)
+    if Keyword.keyword?(attrs) do
+      attrs
+    else
+      raise(ArgumentError,
+            message: "Expected keyword list, got #{inspect(attrs)}")
+    end
   end
 
   defp make_attrs(nil), do: []
-  defp make_attrs(_)  , do: raise(ArgumentError)
+
+  defp make_attrs(arg),
+    do: raise(ArgumentError, "Expected [kw list] or nil, got #{inspect(arg)}")
 
   defp sort_attrs(graph = %{attrs: [_any | _more]}),
     do: %Graph{graph | attrs: Enum.sort(graph.attrs)}
