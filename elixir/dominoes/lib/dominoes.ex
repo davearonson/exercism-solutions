e6b3f6907282a1c4714ccfb31d4404b86500a816
Dave Aronson <davearonson@users.noreply.github.com>
2020-04-16 21:26:12 -0400

    remove redundant comments

--- elixir/dominoes/lib/dominoes.ex
+++ elixir/dominoes/lib/dominoes.ex
@@ -10,19 +10,16 @@ defmodule Dominoes do
   def chain?([{ender, starter} | tail]), do: any_chains?(tail, starter, ender)
 
   defp any_chains?([], starter, starter), do: true
-  defp any_chains?([], _, _), do: false  # redundant but efficient & logical
+  defp any_chains?([], _, _), do: false
   defp any_chains?(pool, starter, ender),
     do: Enum.any?(pool, &do_chain(&1, List.delete(pool, &1), starter, ender))
 
-  # if the first one matches our needs, look for ways to make further progress
   defp do_chain({starter, other}, rest, starter, ender),
     do: any_chains?(rest, other, ender)
 
-  # else if it matches backwards, defer to forward case
   defp do_chain({other, starter}, rest, starter, ender),
     do: do_chain({starter, other}, rest, starter, ender)
 
-  # else this chain attempt is a dead end
   defp do_chain(_, _, _, _), do: false
 
 end
