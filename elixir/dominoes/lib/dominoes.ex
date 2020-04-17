c89c743dd27b1b80fa5e445f0b5faf71772ac215
Dave Aronson <davearonson@users.noreply.github.com>
2020-04-16 20:36:47 -0400

    FINALLY get flash of insight and SIMPLIFY

--- elixir/dominoes/lib/dominoes.ex
+++ elixir/dominoes/lib/dominoes.ex
@@ -7,49 +7,22 @@ defmodule Dominoes do
   """
   @spec chain?(dominoes :: [domino] | []) :: boolean
   def chain?([]), do: true
-  def chain?([{x, x}]), do: true
-  def chain?([{x, y}]) when x != y, do: false
-  def chain?([head | tail]) do 
-    # take these in reverse order of usual search in the rest, because
-    # this is what is *establishing* the parameters, not *fitting* them
-    {must_end, must_match} = head
-    # must start with the "any-izer" else it will glom onto first match,
-    # which may be wrong
-    any_chains?(tail, must_match, must_end)
-  end
+  def chain?([{ender, starter} | tail]), do: any_chains?(tail, starter, ender)
 
-  defp any_chains?(pool, must_match, must_end),
-    do: Enum.any?(pool,
-                  &do_chain([&1 | List.delete(pool, &1)],
-                            must_match, must_end, []))
+  defp any_chains?([], starter, starter), do: true
+  defp any_chains?([], _, _), do: false  # redundant but efficient & logical
+  defp any_chains?(pool, starter, ender),
+    do: Enum.any?(pool, &do_chain(&1, List.delete(pool, &1), starter, ender))
 
-  # if we're down to one, with no rejects, and it matches precisely, either
-  # forward or backward, yes we can chain.  i'm doing it this way rather than
-  # keying off an empty pool, because it's easier to check, rather than keeping
-  # track of the latest status.  i suspect there's some reasonably simple way
-  # to do that but it's just not coming to me.
-  defp do_chain([{must_match, must_end}], must_match, must_end, []), do: true
-  defp do_chain([{must_end, must_match}], must_match, must_end, []), do: true
-
-  # else if we're down to one, and no rejects, it's wrong, so no we can't
-  defp do_chain([_], _must_match, _must_end, []), do: false
-
-  # else if the first one matches our needs, look for ways to make
-  # further progress, including with ones previously rejected
-  defp do_chain([{must_match, other} | tail], must_match, must_end, rejects) do
-    any_chains?(tail ++ rejects, other, must_end)
-  end
+  # if the first one matches our needs, look for ways to make further progress
+  defp do_chain({starter, other}, rest, starter, ender),
+    do: any_chains?(rest, other, ender)
 
   # else if it matches backwards, defer to forward case
-  defp do_chain([{other, must_match} | tail], must_match, must_end, rejects) do
-    do_chain([{must_match, other} | tail], must_match, must_end, rejects)
-  end
+  defp do_chain({other, starter}, rest, starter, ender),
+    do: do_chain({starter, other}, rest, starter, ender)
 
-  # else no match, and more than one, add it to rejects
-  defp do_chain([head | tail], must_match, must_end, rejects) do
-    do_chain(tail, must_match, must_end, [head | rejects])
-  end
-
-  defp do_chain([], _must_match, _must_end, _rejects), do: false
+  # else this chain attempt is a dead end
+  defp do_chain(_, _, _, _), do: false
 
 end
