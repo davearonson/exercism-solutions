75352db6872cd9a8013a1f34f6a106150eefddbe
Dave Aronson <davearonson@users.noreply.github.com>
2020-04-20 09:24:24 -0400

    fix finding nonzero letters in old approach

--- elixir/alphametics/lib/alphametics.ex
+++ elixir/alphametics/lib/alphametics.ex
@@ -17,55 +17,61 @@ defmodule Alphametics do
   """
   @spec solve(puzzle) :: solution | nil
   def solve(puzzle) do
-    letters =
-      puzzle
+    find_solution(puzzle,
+                  extract_letters(puzzle),
+                  extract_nonzero_letters(puzzle),
+                  digits_list())
+  end
+
+  defp extract_letters(puzzle) do
+    puzzle
       |> String.graphemes
       |> Enum.filter(&(&1 >= "A" and &1 <= "Z"))
       |> Enum.uniq
-    nonzero =
-      puzzle
-      |> String.split(" == ")
+  end
+
+  defp extract_nonzero_letters(puzzle) do
+    puzzle
+      |> String.split(" ")
       |> Enum.map(&String.graphemes/1)
       |> Enum.map(&hd/1)
-    digits =
-      (0..9)
-      |> Enum.to_list
-      |> Enum.map(&Integer.to_string/1)
-    puzzle
-    |> find_solution(letters, nonzero, digits)
+      |> Enum.filter(&(&1 >= "A" and &1 <= "Z"))
+  end
+
+  defp digits_list do
+    (0..9)
+    |> Enum.to_list
+    |> Enum.map(&Integer.to_string/1)
   end
 
   defp find_solution(puzzle, letters, nonzero, digits) do
     Enum.find_value(digits,
-                    &do_solve(puzzle, letters, nonzero,
-                              # doing &1 separately and not constructing
-                              # a list shaves about 10% off the time!
-                              &1, List.delete(digits, &1)))
+                    &do_solve(puzzle, letters, nonzero, &1, digits -- [&1]))
   end
 
-  # if we're out of letters, see if it's right
+  defp do_solve(puzzle, [letter | more_letters], nonzero, digit, more_digits) do
+    if digit == "0" and letter in nonzero do
+      nil
+    else
+      new_puzzle = String.replace(puzzle, letter, digit)
+      soln = find_solution(new_puzzle, more_letters, nonzero, more_digits)
+      if soln do
+        Map.put(soln, to_char(letter), String.to_integer(digit))
+      else
+        nil
+      end
+    end
+  end
+
   defp do_solve(puzzle, [], _, _, _) do
     {res, _} = Code.eval_string(puzzle)
     if res, do: %{}, else: nil
   end
 
-  # if the current letter is forbidden to be nonzero,
-  # and the current digit is zero, this branch is a dead end
-  defp do_solve(_, [letter | _], [letter, _], "0", _), do: nil 
-  defp do_solve(_, [letter | _], [_, letter], "0", _), do: nil
-
-  # else recurse; if the final version works, add current letter & digit
-  defp do_solve(puzzle, [letter | more_letters], nonzero,
-                digit, more_digits) do
-    new_puzzle = String.replace(puzzle, letter, digit)
-    sol = find_solution(new_puzzle, more_letters, nonzero, more_digits)
-    if is_nil(sol) do
-      sol
-      else
-        Map.put(sol,
-                letter |> String.to_charlist |> hd,
-                digit |> String.to_integer)
-      end
+  defp to_char(letter) do
+    letter
+    |> String.to_charlist
+    |> hd
   end
 
 end
