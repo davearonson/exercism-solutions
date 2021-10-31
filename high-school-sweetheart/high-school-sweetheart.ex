ec14ab960fec549b910fc8dbc79682344d7a660d
Dave Aronson <davearonson@users.noreply.github.com>
2021-10-31 19:53:07 -0400

    add hs-sweetheart solution

---
+++ high-school-sweetheart/high-school-sweetheart.ex
@@ -0,0 +1,41 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name
+    |> String.trim
+    |> String.graphemes
+    |> hd
+  end
+
+  def initial(name) do
+    letter =
+      name
+      |> first_letter
+      |> String.upcase
+    "#{letter}."
+  end
+
+  def initials(full_name) do
+    full_name
+    |> String.split
+    |> Enum.map(&initial/1)
+    |> Enum.join(" ")
+  end
+
+  def pair(full_name1, full_name2) do
+    ["     ******       ******",
+     "   **      **   **      **",
+     " **         ** **         **",
+     "**            *            **",
+     "**                         **",
+     "**     #{initials(full_name1)}  +  #{initials(full_name2)}     **",
+     " **                       **",
+     "   **                   **",
+     "     **               **",
+     "       **           **",
+     "         **       **",
+     "           **   **",
+     "             ***",
+     "              *\n"]
+    |> Enum.join("\n")
+  end
+end
