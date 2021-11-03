49a6190717a838d57b3ecc00835eed704beacb6a
Dave Aronson <davearonson@users.noreply.github.com>
2021-11-02 20:30:18 -0400

    add date-parser solution

---
+++ elixir/date-parser/date-parser.ex
@@ -0,0 +1,67 @@
+defmodule DateParser do
+  def day() do
+    "\\d{1,2}"
+  end
+
+  def month() do
+    "\\d{1,2}"
+  end
+
+  def year() do
+    "\\d{4}"
+  end
+
+  def day_names() do
+    "[A-Z][a-z]+day"
+  end
+
+  def month_names() do
+    "(January|February|March|April|May|June|July|August|September|October|November|December)"
+
+    # could also do "(May|[A-Z][a-z]{3,})"  ;-)
+  end
+
+  def capture_day() do
+    "(?<day>#{day()})"
+  end
+
+  def capture_month() do
+    "(?<month>#{month()})"
+  end
+
+  def capture_year() do
+    "(?<year>#{year()})"
+  end
+
+  def capture_day_name() do
+    "(?<day_name>#{day_names()})"
+  end
+
+  def capture_month_name() do
+    "(?<month_name>#{month_names()})"
+  end
+
+  def capture_numeric_date() do
+    "#{capture_day()}/#{capture_month()}/#{capture_year()}"
+  end
+
+  def capture_month_name_date() do
+    "#{capture_month_name()} #{capture_day()}, #{capture_year()}"
+  end
+
+  def capture_day_month_name_date() do
+    "#{capture_day_name()}, #{capture_month_name()} #{capture_day()}, #{capture_year()}"
+  end
+
+  def match_numeric_date() do
+    Regex.compile!("^#{capture_numeric_date()}$")
+  end
+
+  def match_month_name_date() do
+    Regex.compile!("^#{capture_month_name_date()}$")
+  end
+
+  def match_day_month_name_date() do
+    Regex.compile!("^#{capture_day_month_name_date()}$")
+  end
+end
