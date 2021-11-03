bec58256df3703b1884a836329eef7bb771b53e9
Dave Aronson <davearonson@users.noreply.github.com>
2021-11-03 19:56:21 -0400

    add rpg-character-sheet solution

---
+++ elixir/rpg-character-sheet/rpg-character-sheet.ex
@@ -0,0 +1,29 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+  end
+
+  def ask_name() do
+    IO.gets("What is your character's name?\n")
+    |> String.trim
+  end
+
+  def ask_class() do
+    IO.gets("What is your character's class?\n")
+    |> String.trim
+  end
+
+  def ask_level() do
+    IO.gets("What is your character's level?\n")
+    |> String.trim
+    |> String.to_integer
+  end
+
+  def run() do
+    welcome()
+    map = %{name:  ask_name(),
+            class: ask_class(),
+            level: ask_level()}
+    IO.inspect(map, label: "Your character")
+  end
+end
