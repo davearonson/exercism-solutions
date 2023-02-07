722ab11e13e5eb254a90ac01e3fcbf2d4da13883
Dave Aronson <davearonson@users.noreply.github.com>
2023-02-07 18:22:36 -0500

    defp, duh

--- elixir/variable-length-quantity/lib/variable_length_quantity.ex
+++ elixir/variable-length-quantity/lib/variable_length_quantity.ex
@@ -9,8 +9,8 @@ defmodule VariableLengthQuantity do
     |> Enum.join
   end
 
-  def do_encode(0), do: <<0>>
-  def do_encode(integer) do
+  defp do_encode(0), do: <<0>>
+  defp do_encode(integer) do
     integer
     |> get_bits
     |> pad_to_mult_of(7, 0)
@@ -20,13 +20,13 @@ defmodule VariableLengthQuantity do
     |> Enum.into(<<>>, fn x -> <<x>> end)
   end
 
-  def get_bits(n) do
+  defp get_bits(n) do
     n
     |> Integer.digits(2)
     |> pad_to_mult_of(8, 0)
   end
 
-  def pad_to_mult_of(list, factor,  what, len \\ false) do
+  defp pad_to_mult_of(list, factor,  what, len \\ false) do
     new_len = if len, do: len, else: length(list)
     if rem(new_len, factor) == 0 do
       list
@@ -35,28 +35,28 @@ defmodule VariableLengthQuantity do
     end
   end
 
-  def all_zeroes(bits), do: Enum.all?(bits, fn bit -> bit == 0 end)
+  defp all_zeroes(bits), do: Enum.all?(bits, fn bit -> bit == 0 end)
 
-  def make_bytes(bytes,         acc \\ [])
-  def make_bytes([],            acc), do: acc
-  def make_bytes([byte],        acc), do: [last_byte(byte) | acc]
-  def make_bytes([byte | rest], acc),
+  defp make_bytes(bytes,         acc \\ [])
+  defp make_bytes([],            acc), do: acc
+  defp make_bytes([byte],        acc), do: [last_byte(byte) | acc]
+  defp make_bytes([byte | rest], acc),
     do: [non_last_byte(byte) | make_bytes(rest, acc)]
 
-  def last_byte(byte) do
+  defp last_byte(byte) do
     byte
     |> pad_to_mult_of(7, 0)
     |> List.insert_at(0, 0)
     |> to_byte
   end
 
-  def non_last_byte(byte) do
+  defp non_last_byte(byte) do
     byte
     |> List.insert_at(0, 1)
     |> to_byte
   end
 
-  def to_byte(bits) do
+  defp to_byte(bits) do
     bits
     |> Enum.join
     |> String.to_integer(2)
@@ -72,10 +72,10 @@ defmodule VariableLengthQuantity do
     |> do_decode([], 0)
   end
 
-  def do_decode([byte|rest], big_acc, lil_acc) when byte >= 128,
+  defp do_decode([byte|rest], big_acc, lil_acc) when byte >= 128,
     do: do_decode(rest, big_acc, lil_acc * 128 + byte - 128)
-  def do_decode([byte|rest], big_acc, lil_acc),
+  defp do_decode([byte|rest], big_acc, lil_acc),
     do: do_decode(rest, [lil_acc * 128 + byte | big_acc], 0)
-  def do_decode([], []     , _lil_acc), do: {:error, "incomplete sequence"}
-  def do_decode([], big_acc, _lil_acc), do: {:ok, Enum.reverse(big_acc) }
+  defp do_decode([], []     , _lil_acc), do: {:error, "incomplete sequence"}
+  defp do_decode([], big_acc, _lil_acc), do: {:ok, Enum.reverse(big_acc) }
 end
